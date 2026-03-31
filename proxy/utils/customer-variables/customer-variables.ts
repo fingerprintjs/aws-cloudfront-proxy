6a93b2cefb8cabd9763b968fe844c8e40f6d287e
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-03-31 15:08:40 +0200

    refactor: remove unused `hasVariable` method from `customer-variables` module

--- proxy/utils/customer-variables/customer-variables.ts
+++ proxy/utils/customer-variables/customer-variables.ts
@@ -34,12 +34,6 @@ export class CustomerVariables {
     }
   }
 
-  async hasVariable(variable: CustomerVariableName): Promise<boolean> {
-    const result = await this.getVariable(variable)
-
-    return result.value !== null
-  }
-
   private async getValueFromProviders<T extends CustomerVariableName>(
     variable: T
   ): Promise<GetVariableResult<T> | null> {
